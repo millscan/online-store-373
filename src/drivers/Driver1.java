d7868fe0538cc5abccaef308aa17c12900206e24
Elan <millscan@email.arizona.edu>
2018-11-02 13:46:43 -0700

    users.csv and driver 1

--- src/drivers/Driver1.java
+++ src/drivers/Driver1.java
@@ -5,7 +5,7 @@ public class Driver1 {
 	
 	public static void main(String args[]) {
 		Store s1 = new Store();
-		Customer c1 = new Customer(s1, "joethebro1998", "joe@example.com", "Joe", "Shmo", "joespass");
+		Customer c1 = new Customer(s1, "joethebro1997", "joe@example.com", "Joe", "Shmo", "joespass");
 		System.out.println(StoreDataIO.storeUserData(c1));
 	}
 	
