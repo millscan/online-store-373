e4781a27b8690bc0ba6f1737172bf6747c4c6846
millscan <33579432+millscan@users.noreply.github.com>
2018-11-06 13:38:55 -0800

    backend and test drivers

--- src/data_storage/StoreDataIO.java
+++ src/data_storage/StoreDataIO.java
@@ -5,8 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import online_store_group_project.User;
+import online_store_group_project.*;
 
 public class StoreDataIO {
 	
@@ -17,6 +18,80 @@ public class StoreDataIO {
 		UsernameInUseError,
 		EmailInUseError
 	}
+	
+	public static ArrayList<User> LoadUsers(Store s) {
+		File usersFile = new File("users.csv");
+		FileInputStream inStream = null;
+		byte userData[] = new byte[1000];
+		String csvText = null;
+		String[] csvLines;
+		ArrayList<User> users = new ArrayList<User>();
+		
+		System.out.println("Loading users...");
+		
+		//make sure file is working
+		if(!usersFile.exists()) {
+			System.out.println("User file not found!!");
+		}
+		
+		//get last version of csv
+		try {
+			inStream = new FileInputStream(usersFile);
+			inStream.read(userData);
+			csvText = (new String(userData)).trim();
+			if(csvText.length() != 0) {
+				csvText += '\n';
+			}
+			csvLines = csvText.split("\n");
+			
+			for(String csvLine : csvLines) {
+				String parsedUserData[] = csvLine.split(",");
+				for(String data : parsedUserData) {
+					switch(parsedUserData[0]) {
+						case "Customer":
+							users.add(new Customer(
+									s,
+									parsedUserData[1], 
+									parsedUserData[2], 
+									parsedUserData[3],
+									parsedUserData[4],
+									parsedUserData[5]
+							));
+							break;
+						case "Owner":
+							users.add(new Owner(
+								s,
+								parsedUserData[1], 
+								parsedUserData[2], 
+								parsedUserData[3],
+								parsedUserData[4],
+								parsedUserData[5]
+							));
+							break;
+						default:
+							break;
+					}
+
+				}
+			}
+			//System.out.println(csvText);
+		}		
+		catch(FileNotFoundException e) {
+			System.out.println("File not found exception");
+		}
+		catch(IOException e) {
+			System.out.println("IO exception");
+		}
+		
+		try {
+			inStream.close();
+		}catch(IOException e){
+			System.out.println("Cannot close user file");
+		}
+		
+		
+		return users;
+	}
 		
 	public static FileErrorCodes storeUserData(User u) {		
 		
@@ -25,6 +100,7 @@ public class StoreDataIO {
 		FileOutputStream outStream = null;
 		String csvLine = new String();
 		byte userData[] = new byte[1000];
+		String userType = u.getClass().getSimpleName();
 		
 		//make sure file is working
 		if(!usersFile.exists()) {
@@ -44,7 +120,7 @@ public class StoreDataIO {
 			if(csvLine.length() != 0) {
 				csvLine += '\n';
 			}
-			System.out.println(csvLine);
+			//System.out.println(csvLine);
 		}
 		catch(FileNotFoundException e) {
 			return FileErrorCodes.FileError;
@@ -70,8 +146,8 @@ public class StoreDataIO {
 		
 		//TODO: Use payson's function to check if username/email exists
 		
-		csvLine = String.format("%s%s,%s,%s,%s,%s",csvLine, u.getUsername(), u.getFirstName(), u.getLastName(), u.getEmailAddress(), u.getPassword());
-		System.out.println(csvLine);
+		csvLine = String.format("%s%s,%s,%s,%s,%s,%s",csvLine, userType, u.getUsername(), u.getFirstName(), u.getLastName(), u.getEmailAddress(), u.getPassword());
+		//System.out.println(csvLine);
 		
 		try {
 			outStream.write(csvLine.getBytes());
---
+++ src/drivers/DataStorageTestDriver.java
@@ -0,0 +1,54 @@
+package drivers;
+
+import online_store_group_project.*;
+
+import java.util.ArrayList;
+
+import data_storage.*;
+
+public class DataStorageTestDriver {
+
+	public static void main(String[] args) {
+		//Create Store   
+		Store s1 = new Store();  
+		
+	    //Create Customers 
+		Customer c1 = new Customer(s1, "joethebro1997", "joe@example.com", "Joe", "Shmo", "joespass");
+		Customer c2 = new Customer(s1, "mattthebro1997", "matt@example.com", "Matt", "Marshall", "mattyice");
+		Customer c3 = new Customer(s1, "Peterthebro1997", "Peter@example.com", "Peter", "Piper", "PipingInTheName");
+		Customer c4 = new Customer(s1, "Jakethebro1997", "Jake@example.com", "Jake", "Snake", "JakeTheSnake");
+		Customer c5 = new Customer(s1, "Christhebro1997", "Chris@example.com", "Chris", "Haavisto", "ChrisTwoFor");
+		
+		//Create Owners 
+		Owner o1 = new Owner(s1, "RobertSacre", "robert@example.com", "Robert", "Sacre", "SacreTheGod"); 
+		Owner o2 = new Owner(s1, "ComputerGuys", "Computers@example.com", "Alexander", "Computer", "CompSciSci"); 
+		Owner o3 = new Owner(s1, "SlickApparel", "FancyApparel@example.com", "Bob", "Aparrel", "CoatsShoesAndDudes"); 
+		Owner o4 = new Owner(s1, "BeautySuppliers", "beautifulwomen@example.com", "Marie", "Taylor", "Beauty123"); 
+		Owner o5 = new Owner(s1, "VideoGameMasters", "VideoJuegos@example.com", "Jimmy", "Carter", "CarterTown"); 
+		
+		//Saving users in users.csv
+		
+		ArrayList<User> usersToSave = new ArrayList<User>();
+		usersToSave.add(c1);
+		usersToSave.add(c2);
+		usersToSave.add(c3);
+		usersToSave.add(c4);
+		usersToSave.add(c5);
+		usersToSave.add(o1);
+		usersToSave.add(o2);
+		usersToSave.add(o3);
+		usersToSave.add(o4);
+		usersToSave.add(o5);
+		
+		for(User u : usersToSave) {
+			StoreDataIO.storeUserData(u);
+		}
+		
+		ArrayList<User> loadedUsers = StoreDataIO.LoadUsers(s1);
+		System.out.println("Printing users:");
+		for(User u : loadedUsers) {
+			System.out.println(u.getUsername());
+		}
+	}
+
+}
